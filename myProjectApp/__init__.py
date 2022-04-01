3e22a3b887edc70e8b83ba1778cca27bb58e46ea
Harsh Raj <harshraj17087@gmail.com>
2022-04-01 11:55:05 +0530

    First commit

---
+++ myProjectApp/__init__.py
@@ -0,0 +1,32 @@
+# This file is underneath myProjectApp/__init__.py
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+app = Flask(__name__)
+
+# ******* LOGIN CONF *********
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+# ****************************
+
+# ******** DATABASE SETUP ********
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+# ********************************
+
+from myProjectApp.core.views import core
+from myProjectApp.error_pages.handlers import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
